bfc866e3995c85bb4c599a5fad1124181345d8a8
JoachimNilsson <joachimnilsson534@gmail.com>
2020-12-25 14:42:59 +0100

    First commit

--- Test/src/test/Test.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-public class Test {
-	public static void main(String[] args){
-		System.out.println("hej");
-		//ClassLoader loader = Test.class.getClassLoader();
-        //System.out.println(loader.getResource("test/Test.class"));
-	}
-}
--- Test/src/test/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author joachimnilsson
- *
- */
-package test;
